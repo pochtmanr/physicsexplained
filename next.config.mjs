1e8bf0959fc536445327a3b65a33ea707eb8c955
Roman Pochtman <pochtmanrca@gmail.com>
2026-04-11 21:43:22 +0200

    chore: wire mdx pipeline with remark-math and rehype-katex

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+import createMDX from "@next/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { strict: false }]],
+  },
+});
+
+export default withMDX(nextConfig);
